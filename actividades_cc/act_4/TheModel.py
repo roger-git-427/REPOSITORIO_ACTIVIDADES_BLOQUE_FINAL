a069b6c1c15a58f8185841add37ae4ec2f03fcd4
Avril <avril03michruiz06@gmail.com>
2025-05-04 23:13:32 -0600

    fix: updated model build

--- actividades_cc/act_4/TheModel.py
+++ actividades_cc/act_4/TheModel.py
@@ -5,12 +5,13 @@ class build:
     def build_it():
         model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+         
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
             tf.keras.layers.MaxPooling2D((2, 2)),
             tf.keras.layers.Dropout(0.25),
 
             tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-            tf.keras.layers.BatchNormalization(),
+            
             tf.keras.layers.MaxPooling2D((2, 2)),
             tf.keras.layers.Dropout(0.25),
 
@@ -25,4 +26,3 @@ class build:
             metrics=['accuracy']
         )
         return model
-    
