758999dbac621ba8870dc472b25161c1054bf06d
Avril <avril03michruiz06@gmail.com>
2025-05-05 22:52:45 -0600

    chore>updated model arquitecture

--- actividades_cc/act_4/TheModel.py
+++ actividades_cc/act_4/TheModel.py
@@ -4,20 +4,13 @@ class build:
     @staticmethod
     def build_it():
         model = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-         
+            tf.keras.Input(shape=(28, 28, 1)),
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
             tf.keras.layers.MaxPooling2D((2, 2)),
             tf.keras.layers.Dropout(0.25),
-
-            tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-            
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Dropout(0.25),
-
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dropout(0.5),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
         model.compile(
